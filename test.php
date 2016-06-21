bc2cf81a5df41958c922c71386dc7b047763f831
ChexWarrior <aplehm@gmail.com>
2016-06-21 18:27:00 -0400

    Update tests

--- test.php
+++ test.php
@@ -3,4 +3,5 @@ include "idgames.php";
 
 $api = new IdGamesApi();
 echo "Ping Server Response: {$api->pingServer()}\n\n";
-echo "DB Ping Server Response: {$api->pingDBServer()}";
+echo "DB Ping Server Response: {$api->pingDBServer()}\n\n";
+echo "Get API Info: {$api->getApiInfo()}";
