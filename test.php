4a95daac31499aa2f89c3307ca2ce3bb9855132d
ChexWarrior <aplehm@gmail.com>
2016-06-25 08:08:22 -0400

    Update test file

--- test.php
+++ test.php
@@ -2,6 +2,13 @@
 include "idgames.php";
 
 $api = new IdGamesApi();
-echo "Ping Server Response: {$api->pingServer()}\n\n";
-echo "DB Ping Server Response: {$api->pingDBServer()}\n\n";
-echo "Get API Info: {$api->getApiInfo()}";
+echo "Ping Server Response: ";
+echo $api->pingServer();
+echo "DB Ping Server Response: ";
+echo $api->pingDBServer();
+echo "Get API Info: "
+echo $api->getApiInfo();
+echo "Get File by ID: ";
+echo $api->getFileById(3);
+echo "Get File by Path: ";
+echo $api->getFileByPath('levels/doom2/megawads/d2reload.zip');
